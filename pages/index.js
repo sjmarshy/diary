ce7626370dbf01cc9eaa8dd26ad46dd075f5ebe2
Sam Marshall <sjmarshy@gmail.com>
2018-04-17 22:45:32 +0100

    feat: add new diary entry

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
 import css from './index.css';
+import Link from 'next/link';
 
 export default () => (
-    <div className={css.container}>
-        <img className={css.image} src="/static/2018-04-08.png" />
-    </div>
+    <React.Fragment>
+        <Link className={css.link} href="/2018-04-08" prefetch>
+            <a>
+                <div className={css.container}>
+                    <img className={css.image} src="/static/2018-04-17.png" />
+                </div>
+            </a>
+        </Link>
+        <p className={css.explainer}>
+            this is my diary - you can click through to older entries. If that
+            stops working, there aren't any more entries
+        </p>
+    </React.Fragment>
 );
